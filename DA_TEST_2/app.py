b1b5bf2f0a3c1d34ae2151c1ad071e6bd3ebd508
DK-Hieu <dk20101995@gmail.com>
2022-05-17 19:01:05 +0700

    push

--- DA_TEST_2/app.py
+++
@@ -1,6 +0,0 @@
-# Importing streamlit
-import streamlit as st
-# Add title and Write in
-st.title('This is our First application with streamlit')
-st.write("Streamlit is an awesome new tool that hepls to solve the problems quickly")
-# pip install streamlit
